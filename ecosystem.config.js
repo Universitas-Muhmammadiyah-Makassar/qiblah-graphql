588df4ea20fc8aa9639b1fb1d750af4e425dfa84
Muhyiddin A M Hayat <muhyiddin@unismuh.ac.id>
2024-08-21 23:35:06 +0800

    chore: Update app.py and ecosystem.config.js to use "qiblah-graphql" as the app name in PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,11 +2,10 @@ module.exports = {
     apps: [
       {
         name: "qiblah-graphql",    // Name of the app in PM2
-        script: "uvicorn",         // Script to run (use uvicorn as the script)
-        args: "app:app --reload --host 0.0.0.0 --port 8118", // Arguments to pass to uvicorn
+        script: "app.js",          // Node.js file to execute
         exec_mode: "cluster",      // Cluster mode for multi-node processes
         instances: 3,              // Number of instances (3 nodes)
-        interpreter: "python3",    // Python interpreter to use
+        interpreter: "node",       // Node.js interpreter to use
         watch: true,               // Enable watch mode (optional, for development)
         autorestart: true,         // Auto-restart if the app crashes
         max_memory_restart: "1G",  // Restart if memory usage exceeds 1GB
